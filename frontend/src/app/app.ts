2f8f6852479fb841b9948e4dc2c5ffc1272de6ef
v-boichenko <vv.boichenko.vv@gmail.com>
2026-01-04 00:13:33 +0100

    Add main page template

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  title = 'QuizApp';
+  gamePin = '';
+  onHostLogin() {
+    console.log("Navigating to Host Login...");
+  }
 }
